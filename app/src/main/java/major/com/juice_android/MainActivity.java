1c0ef372333cbcc03251428da1f2a8863651d21d
Sudin <sudinjoshi13@gmail.com>
2018-12-24 18:32:28 +0545

    Sudin-Toast

--- app/src/main/java/major/com/juice_android/MainActivity.java
+++ app/src/main/java/major/com/juice_android/MainActivity.java
@@ -2,13 +2,18 @@ package major.com.juice_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "My name is Sudin Joshi", Toast.LENGTH_SHORT).show();
 
     }
     public void what()

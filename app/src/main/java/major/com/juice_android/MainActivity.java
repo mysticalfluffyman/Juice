5d4d54b75918c166c0264f0d2d97f8bed39d7a44
Sudin <sudinjoshi13@gmail.com>
2018-12-24 20:39:41 +0545

    Sudin-Register

--- app/src/main/java/major/com/juice_android/MainActivity.java
+++ app/src/main/java/major/com/juice_android/MainActivity.java
@@ -2,10 +2,15 @@ package major.com.juice_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    EditText usernameTxt, nameText, phoneTxt, emailTxt, passwordTxt;
+    Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -13,14 +18,13 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "My name is Sudin Joshi", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "gitlab bois ?", Toast.LENGTH_SHORT).show();
+        usernameTxt = (EditText)findViewById(R.id.usernameText);
+        nameText = (EditText)findViewById(R.id.nameText);
+        phoneTxt = (EditText)findViewById(R.id.phoneText);
+        emailTxt = (EditText)findViewById(R.id.emailText);
+        passwordTxt = (EditText)findViewById(R.id.passwordText);
+        registerBtn = (Button)findViewById(R.id.registerButton);
 
     }
-    public void what()
-    {
-      int a=4;
-    }
 
-    public void pushIt(){ int b=666;}
 }
---
+++ app/src/main/java/major/com/juice_android/model/User.java
@@ -0,0 +1,56 @@
+package major.com.juice_android.model;
+
+public class User
+{
+    private String username, name, password, phone, email;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+}

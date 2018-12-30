9bc76e824e989dfa35ad0cb7badaf79791d552d9
Subigya <gautamsubigya@gmail.com>
2018-12-30 14:17:09 +0545

    Fixed Dependencies issues

--- app/src/main/java/major/com/juice_android/MainLayout.java
+++ app/src/main/java/major/com/juice_android/MainLayout.java
@@ -11,7 +11,7 @@ public class MainLayout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
 
         mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
     }

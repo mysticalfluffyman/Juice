c91c0fe84284e6f0f603d7b6029cf214e7b54ab3
Subigya <gautamsubigya@gmail.com>
2019-01-26 22:30:29 +0545

    Layouts1

--- app/src/main/java/major/com/juice_android/MainActivity.java
+++ app/src/main/java/major/com/juice_android/MainActivity.java
@@ -26,120 +26,120 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nowplaying);
 
-        usernameTxt = (EditText)findViewById(R.id.usernameText);
-        nameTxt = (EditText)findViewById(R.id.nameText);
-        phoneTxt = (EditText)findViewById(R.id.phoneText);
-        emailTxt = (EditText)findViewById(R.id.emailText);
-        passwordTxt = (EditText)findViewById(R.id.passwordText);
-        registerBtn = (Button)findViewById(R.id.registerButton);
-        goToLoginText = (TextView)findViewById(R.id.goToLogin);
-
-        registerBtn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-               registerUser();
-            }
-        });
-        goToLoginText.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                goToLoginPage();
-            }
-        });
+//        usernameTxt = (EditText)findViewById(R.id.usernameText);
+//        nameTxt = (EditText)findViewById(R.id.nameText);
+//        phoneTxt = (EditText)findViewById(R.id.phoneText);
+//        emailTxt = (EditText)findViewById(R.id.emailText);
+//        passwordTxt = (EditText)findViewById(R.id.passwordText);
+//        registerBtn = (Button)findViewById(R.id.registerButton);
+//        goToLoginText = (TextView)findViewById(R.id.goToLogin);
+//
+//        registerBtn.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View view)
+//            {
+//               registerUser();
+//            }
+//        });
+//        goToLoginText.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                goToLoginPage();
+//            }
+//        });
 
     }
 
-    public void registerUser()
-    {
-        String usernameValue = usernameTxt.getText().toString();
-        String nameValue = nameTxt.getText().toString();
-        String phoneValue = phoneTxt.getText().toString();
-        String emailValue = emailTxt.getText().toString();
-        String passwordValue = passwordTxt.getText().toString();
-
-        if (usernameValue.isEmpty())
-        {
-            usernameTxt.setError("Username is required");
-            usernameTxt.requestFocus();
-            return;
-        }
-        if (emailValue.isEmpty())
-        {
-            emailTxt.setError("Email is required");
-            emailTxt.requestFocus();
-            return;
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(emailValue).matches())
-        {
-            emailTxt.setError("Enter a valid E-mail");
-            emailTxt.requestFocus();
-            return;
-        }
-        if (passwordValue.isEmpty())
-        {
-            passwordTxt.setError("Password is required");
-            passwordTxt.requestFocus();
-            return;
-        }
-        if (passwordValue.length() < 6)
-        {
-            passwordTxt.setError("Password should be more than 6 characters long");
-            passwordTxt.requestFocus();
-            return;
-        }
-        if (nameValue.isEmpty())
-        {
-            nameTxt.setError("Name is required");
-            nameTxt.requestFocus();
-            return;
-        }
-        if (phoneValue.isEmpty())
-        {
-            phoneTxt.setError("Phone is required");
-            phoneTxt.requestFocus();
-            return;
-        }
-
-        Call<DefaultResponse> call = RetrofitClient.getInstance().getApi().createUser(usernameValue, nameValue, emailValue, passwordValue, phoneValue);
-        call.enqueue(new Callback<DefaultResponse>()
-        {
-            @Override
-            public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response)
-            {
-                DefaultResponse defaultResponse = response.body();
-                if (response.code() == 201)
-                {
-                    Toast.makeText(MainActivity.this, defaultResponse.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-                else if (response.code() == 422)
-                {
-                    Toast.makeText(MainActivity.this, "User Already Exists" , Toast.LENGTH_SHORT).show();
-                }
-                else if (response.code() == 421)
-                {
-                    Toast.makeText(MainActivity.this, "Some Error Occured", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<DefaultResponse> call, Throwable t)
-            {
-
-            }
-        });
-    }
-
-    public void goToLoginPage()
-    {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
+//    public void registerUser()
+//    {
+//        String usernameValue = usernameTxt.getText().toString();
+//        String nameValue = nameTxt.getText().toString();
+//        String phoneValue = phoneTxt.getText().toString();
+//        String emailValue = emailTxt.getText().toString();
+//        String passwordValue = passwordTxt.getText().toString();
+//
+//        if (usernameValue.isEmpty())
+//        {
+//            usernameTxt.setError("Username is required");
+//            usernameTxt.requestFocus();
+//            return;
+//        }
+//        if (emailValue.isEmpty())
+//        {
+//            emailTxt.setError("Email is required");
+//            emailTxt.requestFocus();
+//            return;
+//        }
+//        if (!Patterns.EMAIL_ADDRESS.matcher(emailValue).matches())
+//        {
+//            emailTxt.setError("Enter a valid E-mail");
+//            emailTxt.requestFocus();
+//            return;
+//        }
+//        if (passwordValue.isEmpty())
+//        {
+//            passwordTxt.setError("Password is required");
+//            passwordTxt.requestFocus();
+//            return;
+//        }
+//        if (passwordValue.length() < 6)
+//        {
+//            passwordTxt.setError("Password should be more than 6 characters long");
+//            passwordTxt.requestFocus();
+//            return;
+//        }
+//        if (nameValue.isEmpty())
+//        {
+//            nameTxt.setError("Name is required");
+//            nameTxt.requestFocus();
+//            return;
+//        }
+//        if (phoneValue.isEmpty())
+//        {
+//            phoneTxt.setError("Phone is required");
+//            phoneTxt.requestFocus();
+//            return;
+//        }
+//
+//        Call<DefaultResponse> call = RetrofitClient.getInstance().getApi().createUser(usernameValue, nameValue, emailValue, passwordValue, phoneValue);
+//        call.enqueue(new Callback<DefaultResponse>()
+//        {
+//            @Override
+//            public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response)
+//            {
+//                DefaultResponse defaultResponse = response.body();
+//                if (response.code() == 201)
+//                {
+//                    Toast.makeText(MainActivity.this, defaultResponse.getMessage(), Toast.LENGTH_SHORT).show();
+//                }
+//                else if (response.code() == 422)
+//                {
+//                    Toast.makeText(MainActivity.this, "User Already Exists" , Toast.LENGTH_SHORT).show();
+//                }
+//                else if (response.code() == 421)
+//                {
+//                    Toast.makeText(MainActivity.this, "Some Error Occured", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<DefaultResponse> call, Throwable t)
+//            {
+//
+//            }
+//        });
+//    }
+//
+//    public void goToLoginPage()
+//    {
+//        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
 
 }

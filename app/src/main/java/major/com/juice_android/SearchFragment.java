c75141d7555d24f0cdb7ea3e291704ddd7f4ca38
Sudin <sudinjoshi13@gmail.com>
2019-01-29 19:27:08 +0545

    Sudin-Final commit

--- app/src/main/java/major/com/juice_android/SearchFragment.java
+++ app/src/main/java/major/com/juice_android/SearchFragment.java
@@ -1,6 +1,6 @@
 package major.com.juice_android;
 
-import android.os.Bundle;
+import  android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
